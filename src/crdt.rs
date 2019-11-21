9822bdccaaecc19f57151d507c19a5ea7f7027aa
Robert Lord <robert@lord.io>
2019-11-21 00:02:37 -0500

    edit and recalculate implemented

--- src/crdt.rs
+++ src/crdt.rs
@@ -1,34 +1,57 @@
 use crate::tree::Tree;
-use std::collections::BTreeMap;
 
 pub trait Edit<State> {
     fn apply(&self, tree: &mut State);
 }
 
-pub struct OpSetCrdt<E: Edit<State> + Ord, State: Clone> {
+pub struct OpSetCrdt<E: Edit<S> + Ord, S: Clone> {
     /// list of all edits applied to this tree
     edits: Vec<E>,
-    /// current state of the tree
-    state: State,
-    /// maps (num edits applied) -> (tree at that point in time)
-    old_states: BTreeMap<usize, State>,
+    /// a list of (num edits applied, state of tree at that point in time)
+    states: Vec<(usize, S)>,
+    /// at most, this many edits will be skipped over in the states cache
+    cache_gap: usize,
 }
 
-impl <E: Edit<State> + Ord, State: Clone> OpSetCrdt<E, State> {
-    pub fn new(initial_state: State) -> Self {
+impl <E: Edit<S> + Ord, S: Clone> OpSetCrdt<E, S> {
+    pub fn new(initial_state: S, cache_gap: usize) -> Self {
         OpSetCrdt {
             edits: Vec::new(),
-            state: initial_state,
-            old_states: BTreeMap::new(),
+            cache_gap,
+            states: vec![(0, initial_state)],
         }
     }
 
     pub fn edit(&mut self, edit: E) {
-        unimplemented!();
+        let insert_point = self.edits.binary_search(&edit).expect_err("two edits had the same timestamp");
+        self.edits.insert(insert_point, edit);
+        self.recalculate(insert_point);
     }
 
-    pub fn state(&self) -> &State {
-        &self.state
+    /// Recalculates states after the edit list has been changed. The first `insert_point`
+    /// edits should be identical to the last time `recalculate` was called.
+    fn recalculate(&mut self, insert_point: usize) {
+        let index_of_first_bad_state = match self.states.binary_search_by_key(&insert_point, |(n, _)| *n) {
+            Ok(n) => n+1,
+            Err(n) => n,
+        };
+        // delete all previous states after least_insert_point, add one so that if something exists
+        // exactly at `least_insert_point` it is preserved.
+        self.states.truncate(index_of_first_bad_state);
+        let (mut applied_edits, mut state) = self.states.pop().unwrap();
+        while applied_edits < self.edits.len() {
+            if self.states.len() == 0 || self.states.last().unwrap().0 + self.cache_gap > applied_edits {
+                // time to insert a new cache
+                self.states.push((applied_edits, state.clone()));
+            }
+            self.edits[applied_edits].apply(&mut state);
+            applied_edits += 1;
+        }
+        self.states.push((applied_edits, state));
+    }
+
+    pub fn state(&self) -> &S {
+        &self.states.last().expect("somehow state cache was empty?").1
     }
 }
 
