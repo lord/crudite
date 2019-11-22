c0ac663847de10f04e632d001b7a75f39d9e433b
Robert Lord <robert@lord.io>
2019-11-22 01:59:24 -0500

    document

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
 pub mod opset;
 pub mod tree;
+
+mod document;
+use document::*;
--- src/opset.rs
+++ src/opset.rs
@@ -1,22 +1,20 @@
-use crate::tree::Tree;
-
-pub trait Edit<State> {
+pub trait Operation<State> {
     fn apply(&self, tree: &mut State);
 }
 
-pub struct Opset<E: Edit<S> + Ord, S: Clone> {
-    /// list of all edits applied to this tree
-    edits: Vec<E>,
-    /// a list of (num edits applied, state of tree at that point in time)
+pub struct Opset<E: Operation<S> + Ord, S: Clone> {
+    /// list of all ops applied to this tree
+    ops: Vec<E>,
+    /// a list of (num ops applied, state of tree at that point in time)
     states: Vec<(usize, S)>,
-    /// at most, this many edits will be skipped over in the states cache
+    /// at most, this many ops will be skipped over in the states cache
     cache_gap: usize,
 }
 
-impl<E: Edit<S> + Ord, S: Clone> Opset<E, S> {
+impl<E: Operation<S> + Ord, S: Clone> Opset<E, S> {
     pub fn new(initial_state: S, cache_gap: usize) -> Self {
         Opset {
-            edits: Vec::new(),
+            ops: Vec::new(),
             cache_gap,
             states: vec![(0, initial_state)],
         }
@@ -24,21 +22,21 @@ impl<E: Edit<S> + Ord, S: Clone> Opset<E, S> {
 
     pub fn edit(&mut self, edit: E) {
         let insert_point = self
-            .edits
+            .ops
             .binary_search(&edit)
-            .expect_err("two edits had the same timestamp");
-        self.edits.insert(insert_point, edit);
+            .expect_err("two ops had the same timestamp");
+        self.ops.insert(insert_point, edit);
         self.recalculate(insert_point);
     }
 
-    pub fn edit_from_iter<I: std::iter::Iterator<Item = E>>(&mut self, edits: I) {
+    pub fn edit_from_iter<I: std::iter::Iterator<Item = E>>(&mut self, ops: I) {
         let mut least_insert_point = None;
-        for edit in edits {
+        for edit in ops {
             let insert_point = self
-                .edits
+                .ops
                 .binary_search(&edit)
-                .expect_err("two edits had the same timestamp");
-            self.edits.insert(insert_point, edit);
+                .expect_err("two ops had the same timestamp");
+            self.ops.insert(insert_point, edit);
             least_insert_point = match least_insert_point {
                 Some(prev) if prev < insert_point => Some(prev),
                 _ => Some(insert_point),
@@ -50,7 +48,7 @@ impl<E: Edit<S> + Ord, S: Clone> Opset<E, S> {
     }
 
     /// Recalculates states after the edit list has been changed. The first `insert_point`
-    /// edits should be identical to the last time `recalculate` was called.
+    /// ops should be identical to the last time `recalculate` was called.
     fn recalculate(&mut self, insert_point: usize) {
         let index_of_first_bad_state =
             match self.states.binary_search_by_key(&insert_point, |(n, _)| *n) {
@@ -60,18 +58,18 @@ impl<E: Edit<S> + Ord, S: Clone> Opset<E, S> {
         // delete all previous states after least_insert_point, add one so that if something exists
         // exactly at `least_insert_point` it is preserved.
         self.states.truncate(index_of_first_bad_state);
-        let (mut applied_edits, mut state) = self.states.pop().unwrap();
-        while applied_edits < self.edits.len() {
+        let (mut applied_ops, mut state) = self.states.pop().unwrap();
+        while applied_ops < self.ops.len() {
             if self.states.len() == 0
-                || self.states.last().unwrap().0 + self.cache_gap <= applied_edits
+                || self.states.last().unwrap().0 + self.cache_gap <= applied_ops
             {
                 // time to insert a new cache
-                self.states.push((applied_edits, state.clone()));
+                self.states.push((applied_ops, state.clone()));
             }
-            self.edits[applied_edits].apply(&mut state);
-            applied_edits += 1;
+            self.ops[applied_ops].apply(&mut state);
+            applied_ops += 1;
         }
-        self.states.push((applied_edits, state));
+        self.states.push((applied_ops, state));
     }
 
     pub fn state(&self) -> &S {
@@ -93,14 +91,14 @@ mod test {
         value: usize,
     }
 
-    impl Edit<Vec<usize>> for TestEdit {
+    impl Operation<Vec<usize>> for TestEdit {
         fn apply(&self, state: &mut Vec<usize>) {
             state.push(self.value);
         }
     }
 
     #[test]
-    fn various_edits_work() {
+    fn various_ops_work() {
         let mut crdt = Opset::new(vec![0], 2);
         // initial edit
         crdt.edit(TestEdit {
@@ -144,10 +142,10 @@ mod test {
     }
 
     #[test]
-    fn various_edits_work_with_iter() {
+    fn various_ops_work_with_iter() {
         let mut crdt = Opset::new(vec![0], 2);
 
-        let edits = vec![
+        let ops = vec![
             TestEdit {
                 timestamp: 10,
                 value: 1,
@@ -169,7 +167,7 @@ mod test {
                 value: 5,
             },
         ];
-        crdt.edit_from_iter(edits.into_iter());
+        crdt.edit_from_iter(ops.into_iter());
         assert_eq!(crdt.state(), &[0, 2, 1, 5, 4, 3]);
         assert_eq!(crdt.states.len(), 4);
     }
