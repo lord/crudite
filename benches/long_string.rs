0328ba995354db09aa1c3ae4836835dac03a9534
Robert Lord <robert@lord.io>
2019-11-27 12:34:49 -0800

    benchmark done properly

--- benches/long_string.rs
+++ benches/long_string.rs
@@ -6,44 +6,39 @@ use crudite::*;
 use criterion::Criterion;
 use criterion::black_box;
 
-fn character_insert_bench(n: usize) -> Doc {
-    let mut doc = Doc::new();
-    doc.update(DocOp {
-        timestamp: 1,
-        edits: vec![
-            tree::Edit::TextCreate{
-                id: Id {num: 1},
-            },
-            tree::Edit::MapInsert{
-                parent: ROOT_ID,
-                key: "my key".to_string(),
-                item: tree::Value::Collection(Id {num: 1}),
-            },
-        ],
-    });
-
-    for i in 2..n {
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("character insert", |b| {
+        let mut doc = Doc::new();
         doc.update(DocOp {
-            timestamp: i as u64,
+            timestamp: 1,
             edits: vec![
-                black_box(tree::Edit::TextInsert {
-                    prev: Id {num: i-1},
-                    id: Id {num: i},
-                    character: if i % 2 == 0 {'a'} else {'b'},
-                }),
+                tree::Edit::TextCreate{
+                    id: Id {num: 1},
+                },
+                tree::Edit::MapInsert{
+                    parent: ROOT_ID,
+                    key: "my key".to_string(),
+                    item: tree::Value::Collection(Id {num: 1}),
+                },
             ],
         });
-    }
 
-    doc
-}
-
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("character insert x 1", |b| b.iter(|| character_insert_bench(1)));
-    c.bench_function("character insert x 10", |b| b.iter(|| character_insert_bench(10)));
-    c.bench_function("character insert x 100", |b| b.iter(|| character_insert_bench(100)));
-    c.bench_function("character insert x 1_000", |b| b.iter(|| character_insert_bench(1_000)));
-    c.bench_function("character insert x 10_000", |b| b.iter(|| character_insert_bench(10_000)));
+        let mut i = 2;
+        b.iter(|| {
+            doc.update(DocOp {
+                timestamp: i as u64,
+                edits: vec![
+                    black_box(tree::Edit::TextInsert {
+                        prev: Id {num: i-1},
+                        id: Id {num: i},
+                        character: if i % 2 == 0 {'a'} else {'b'},
+                    }),
+                ],
+            });
+            i += 1;
+        });
+        black_box(doc);
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
